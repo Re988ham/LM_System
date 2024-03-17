e9dbc8d7e1d05a3d2e90eeea907aaa73d9ecd993
Mahran_AD <mhranabwdqt971@gmail.com>
2024-03-17 13:20:21 +0300

    Add input fields to register form.

--- app/Http/Controllers/Dashboard/RegisterController.php
+++ app/Http/Controllers/Dashboard/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
@@ -11,16 +12,22 @@ class RegisterController extends Controller
 {
     public function create()
     {
-        return view('session.register');
+        return view('dashboard.session.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        $attributes = request()->validate([
+//        dd($request->date);
+        $attributes = $request->validate([
             'name' => ['required', 'max:50'],
             'email' => ['required', 'email', 'max:50', Rule::unique('users', 'email')],
             'password' => ['required', 'min:5', 'max:20'],
             'agreement' => ['accepted'],
+            'address' => ['required', 'max:50'],
+            'mobile_number' => ['required', 'digits:10', 'starts_with:09', 'unique:users,mobile_number'],
+            'gender' => ['required', 'in:male,female'],
+            'date' => ['required', 'date_format:Y-m-d'],// 'date_format:Y-m-d'
+//            'image' => ['required', 'image', 'max:2048', 'mimes:jpeg,png'],
         ]);
 
         $attributes['password'] = bcrypt($attributes['password']);
