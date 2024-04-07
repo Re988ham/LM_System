4ed62d806f9982a53f8ffd293bed65e155a58c08
Re988ham <r33474655@gmail.com>
2024-04-06 17:03:54 -0700

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-<<<<<<< HEAD
+
 use App\Models\Specialization;
-=======
->>>>>>> ed1f74a1f0c876f204dcb737ef14993d567efc72
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,28 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
-       
+
+
         $this->call([
 
             UserSeeder::class,
             RoleAndPermissionSeeder::class,
             SpecializationSeeder::class,
             CountrySeeder::class,
-=======
-        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([
-
-            CategorySeeder::class,
-            UserSeeder::class,
-//            RoleAndPermissionSeeder::class,
-
->>>>>>> ed1f74a1f0c876f204dcb737ef14993d567efc72
         ]);
     }
 }
