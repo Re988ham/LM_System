3f872fa801616d416b1e00c48c4728f69f1aad87
Re988ham <r33474655@gmail.com>
2024-04-09 14:30:24 -0700

    Fix database and seeder

--- app/Models/SpecializationOfUser.php
+++ app/Models/SpecializationOfUser.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SpecializationOfUser extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = [
         'user_id',
         'specialization_id'
