a60d9f42ddce939c1cb52f8b0cd1b32aa9779e03
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-08-15 14:37:45 +0300

    Rename improvements

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Reports;
+use App\Models\Report;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class ReportsController extends Controller
+class ReportController extends Controller
 {
     public function send_reports(Request $request){
         $user_id=Auth::user()->id;
         $data['report']=$request->report;
         $data['user_id']=$user_id;
 
-        return Reports::create($data);
+        return Report::create($data);
     }
 }
