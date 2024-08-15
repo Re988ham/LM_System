373a3eb12e221ff3d0b9e8f6564a92de37cd92f4
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-08-15 14:36:41 +0300

    Some fixes in migrations and models.

--- database/migrations/2024_08_14_171533_create_reports_table.php
+++ database/migrations/2024_08_14_171533_create_reports_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->string('report');
+            $table->text('report');
             $table->timestamps();
         });
     }
